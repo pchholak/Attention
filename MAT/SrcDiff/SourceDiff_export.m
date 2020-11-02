fbc6e69ac35117c2b5685892684dd64f9d8a4d7f
Parth Chholak <parthchholak@gmail.com>
2020-11-02 17:07:51 +0530

    src diff

--- MAT/SrcDiff/SourceDiff_export.m
+++ MAT/SrcDiff/SourceDiff_export.m
@@ -2,22 +2,22 @@
 % the Test source time average file as 'xyz' and changing 'sub'
 
 %% Given
-sub = 6;
-info.res_path_coh = '/home/rommel/research/results/perception/Coherence/';
+sub = 5;
+info.res_path_coh = 'D:/Research/Academic/PhD/Attention/Coherence';
 keywd = 'Coh(120by3s)';
 
 %% Locate and list .mat files
-fmatF = dir([info.res_path_coh, keywd, '*_sub', sprintf('%02d',sub), '_F.mat']);
+fmatI = dir([info.res_path_coh, keywd, '*_Subject', sprintf('%02d',sub), '_I.mat']);
 fmatB = dir([info.res_path_coh, '*_sub', sprintf('%02d',sub), '_B.mat']);
 
 %% Load files, average and take difference
-load([info.res_path_coh, fmatF(1).name]); clear fmatF
-srcm = mean(src, 2); srcF = [srcm, srcm];
+load([info.res_path_coh, fmatI(1).name]); clear fmatI
+srcm = mean(src, 2); srcI = [srcm, srcm];
 load([info.res_path_coh, fmatB(1).name]); clear fmatB
 srcm = mean(src, 2); srcB = [srcm, srcm];
-img = srcF-srcB;
+img = srcI-srcB;
 
 %% Prepare to export
 xyz.ImageGridAmp = img;
-xyz.Comment = ['Diff_' keywd '_sub' sprintf('%02d',sub)];
+xyz.Comment = ['Diff_' keywd '_Subject' sprintf('%02d',sub)];
 clear info
