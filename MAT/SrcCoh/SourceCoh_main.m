7f28169cdc6a29a38d05c395b2622d5eb72c5a13
Parth Chholak <parthchholak@gmail.com>
2020-11-02 16:57:13 +0530

    src coh

--- MAT/SrcCoh/SourceCoh_main.m
+++ MAT/SrcCoh/SourceCoh_main.m
@@ -5,9 +5,9 @@ clc
 % Given
 iSubj = 5;
 f1 = 120/9;
-info.root_to_stripped = 'D:/Research/Academic/PhD/Attention/Stripped'
+info.root_to_stripped = 'D:/Research/Academic/PhD/Attention/Stripped';
 info.res_path_coh = 'D:/Research/Academic/PhD/Attention/Coherence';
 
-info.name = sprintf('sub%02d', iSubj);
+info.name = sprintf('Subject%02d', iSubj);
 SourceCoh_cond(info, 'B', f1)
 SourceCoh_cond(info, 'I', f1)
--- MAT/StripTrials/strip_main.m
+++ MAT/StripTrials/strip_main.m
@@ -8,7 +8,7 @@ info.root = 'D:/brainstorm_db/Perception/data';
 info.root_to_stripped = 'D:/Research/Academic/PhD/Attention/Stripped';
 
 % Strip 'B' and 'I' trials and get Kernel matrix
-info.name = sprintf('Subject%02d', iSubj)
+info.name = sprintf('Subject%02d', iSubj);
 info.folder = 'run_percep_tsss_notch';
 strip_trials(info, 'B', 1)
 strip_trials(info, 'I')
